78e1c72e1fdf6a688f5af5cbb7f348006fdd8c49
reeseroo <reesewelty@utexas.edu>
2022-03-24 16:22:50 -0500

    Create countUpwardsByThree_HW.js

---
+++ IntroToJavascript/countUpwardsByThree_HW.js
@@ -0,0 +1,10 @@
+//write a funtion that counts from 0 to 30 incrementing by 3
+// output: each number to terminal
+
+function countUpwardsByThree(){
+    for (var i=0; i <= 30; i = i+3){
+        console.log(i);
+    }
+}
+
+countUpwardsByThree();
